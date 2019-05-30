cc3d402c9a07a7f9fb49e015e9bc530e95d36178
isthgtran <an_tr@antr.fritz.box>
2019-05-30 20:01:53 +0200

    frontend: add ugly dropdown suggestion

--- frontend/search.js
+++ frontend/search.js
@@ -4,14 +4,27 @@ function next(){
     location.href = "./quiz.html";
 }
 
+function delete_suggestions(list){
+    while(list.firstChild){
+        list.removeChild(list.firstChild);
+    }
+}
+
 async function fetchSuggestions(input){
-    console.log('type');
+    let suggestion_list = document.getElementById('filter');
+    delete_suggestions(suggestion_list);
+
     const response = await fetch(search_url+`?fuzzy=${input.value}`);
     let data = await response.json();
-    alert(data);
-    // data.forEach( (el) => {
-    //     const {label, url} = el;
-    //     const fact = el.class;
-    //     alert(fact + label + url);
-    // });
+
+    data.forEach( (el) => {
+        const {label, url} = el;
+        const fact = el.class;
+        let option = document.createElement('option');
+
+        option.textContent = label+ '\t' + fact;
+        option.value = label;
+
+        suggestion_list.appendChild(option);
+    });
 }
