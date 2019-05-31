490a8c7165757422d3b0e0775007a1f2ba28a931
Moritz Klein <moritz.klein@protonmail.com>
2019-05-31 14:54:02 +0200

    Merge..?

--- frontend/quiz.js
+++ frontend/quiz.js
@@ -1,34 +1,4 @@
 let base_url = "http://localhost:5000/api/fact";
-<<<<<<< HEAD
-// let base_url = "http://192.168.99.100:5000/api/fact";
-
-async function getFacts2(){
-    const response = await fetch(base_url);
-    const data = await response.json();
-
-    data.forEach(el=>{
-        const {Fact} = el;
-        const {factTrue, object, predicate, resource, subject} = Fact;
-    });
-}
-
-// async function getFacts(){
-//     await $.ajax({
-//         url: base_url,
-//         type: "get",
-//         beforeSend: () => {
-//             // show gif
-//         },
-//         success: (response) => {
-//             alert(response)
-//             console.log("Received response: ", response);
-//         },
-//         error: function (jqXHR, textStatus, errorThrow) {
-//             alert(textStatus);
-//         }
-//     });
-// }
-=======
 
 window.onload = ()=> {
     let loader = document.getElementById('loader');
@@ -94,4 +64,3 @@ async function fetchFacts(){
         fact_buttons[index].value = factTrue;
     });
 }
->>>>>>> origin/elasticSearch
